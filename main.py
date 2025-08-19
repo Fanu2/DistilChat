1a143fe6f494fa03a1f216038dc7584172843388
JasCoder <85804831+Fanu2@users.noreply.github.com>
2025-08-19 12:16:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-from app.main import app
+from fastapi import FastAPI
+
+app = FastAPI()
+
+# Now put your endpoints below, e.g.:
+@app.get("/")
+def read_root():
+    return {"message": "Hello world!"}
