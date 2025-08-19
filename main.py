43b10bdd3e2cf268116d59e215e997dd3186eda7
JasCoder <85804831+Fanu2@users.noreply.github.com>
2025-08-19 12:31:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI
 
-app = FastAPI()
-
-# Now put your endpoints below, e.g.:
 @app.get("/")
-def read_root():
-    return {"message": "Hello world!"}
+async def root():
+    return {"status": "ok"}
+
